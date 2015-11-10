972611443b0bcb10e711bcf60b615e36daed3d89
Julien Chantereau <jchantereau@gmail.com>
2015-11-10 17:14:13 +0100

    edit profile custom fields

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone_number, :address)
+  end
+end
