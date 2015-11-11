578b7b3b7c9a115a2cbb23150c92f52787742507
Thomas Joët <thomas.joet.fr@gmail.com>
2015-11-11 10:39:19 +0100

    added find_gift and create methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,21 @@
 class BookingsController < ApplicationController
+  before_action :find_gift, only: [ :new, :create ]
 
   def new
-    @booking = Booking.new(gift_id: params["gift"])
+
+  end
+
+  def create
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:message, :friend_id, :gift_id, :user_id)
+  end
+
+  def find_gift
+    @gift = Gift.find(params["gift"])
   end
 
 end
